d0c22703b272a312dc0a42231ddcf4a3bf9c0131
IgorAbreu <igor29nahan@gmail.com>
2023-12-01 18:42:38 -0300

    refactor: chaging image component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,16 @@
-import { Animals } from './images/Animals'
-
+import Image from 'next/image'
+import Animals from '../assets/animals.png'
 export function Hero() {
   return (
     <div className="flex w-full items-center justify-between">
       <h1 className="w-[30rem] text-7xl font-bold">
         Leve a felicidade para o seu lar
       </h1>
-      <Animals />
+      <Image
+        src={Animals}
+        width={600}
+        alt="Image about animals of differents kinds"
+      />
     </div>
   )
 }
