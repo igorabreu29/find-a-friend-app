6dff31b10466b3077c74b009914f74cf03a7bc76
IgorAbreu <igor29nahan@gmail.com>
2023-12-01 18:38:58 -0300

    refactor: removing white space

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Hero } from '@/components/Hero'
 
 export default function Home() {
   return (
-    <div className="bg-primary-light flex min-h-screen flex-col items-start justify-start gap-20 p-24">
+    <div className="flex min-h-screen flex-col items-start justify-start gap-20 bg-primary-light p-24">
       <Header />
       <Hero />
       <Footer />
